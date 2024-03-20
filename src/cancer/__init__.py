9e7d4386fe7bfa79cf7e92ae7712b16e2906223e
prasad535 <prasad.nagineni12345@gmail.com>
2024-03-21 04:52:42 +0530

    setup.py updated

---
+++ src/cancer/__init__.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import logging
+
+LOGGING_STR = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+LOG_DIR = "logs"
+log_file_path = os.path.join(log_dir,"history_logs.log")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= LOGGING_STR,
+
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("cancerLogger")
